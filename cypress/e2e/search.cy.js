bcc24e2815d54541ca2851237ff3db7acc1a161b
Александр <soedinennye@gmail.com>
2025-02-13 16:16:31 +0700

    search

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,5 +1,7 @@
 describe('Регистрация пользователя', () => {
   it('Позитивный сценарий: успешная регистрация', () => {
     cy.login();
+    cy.get('input[name="q"]').type('Тест Имя');
+    cy.get('button[class="btn btn-submit"]').click();
   });
 });
